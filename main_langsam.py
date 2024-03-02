7434ac6c8431cdbcf61b6f31bbad3c42c0a7f8d4
PSRahul <psrahul.ml@gmail.com>
2024-03-02 18:11:22 +0100

    dependency are now fixed. The code flows through

--- main_langsam.py
+++ main_langsam.py
@@ -3,5 +3,6 @@ from lang_sam import LangSAM
 
 model = LangSAM()
 image_pil = Image.open("sample_input_images/chair.jpg").convert("RGB")
-text_prompt = "wheel"
+text_prompt = "chair"
 masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
+print(len(masks))
