42a88f8aa813d1a6ef45e3c66c856e518297f398
PSRahul <psrahul.ml@gmail.com>
2024-03-02 18:18:23 +0100

    mask generation works

--- main_langsam.py
+++ main_langsam.py
@@ -1,8 +1,12 @@
 from PIL import Image
 from lang_sam import LangSAM
-
+import matplotlib.pyplot as plt
+ 
 model = LangSAM()
 image_pil = Image.open("sample_input_images/chair.jpg").convert("RGB")
 text_prompt = "chair"
 masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
+masks=masks.numpy()[0] *1
+plt.imshow(masks)
+plt.show()
 print(len(masks))
