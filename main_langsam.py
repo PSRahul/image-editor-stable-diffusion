f0553b41177ff6a26c5f2c42db3409b0e375b28d
PSRahul <psrahul.ml@gmail.com>
2024-03-02 22:12:05 +0100

    change configuration to hydra

--- main_langsam.py
+++ main_langsam.py
@@ -2,23 +2,28 @@ from PIL import Image
 from lang_sam import LangSAM
 import matplotlib.pyplot as plt
 import numpy as np
+import hydra
 
-model = LangSAM()
-image_pil = Image.open("sample_input_images/chair.jpg")
-text_prompt = "chair"
-masks, boxes, phrases, logits = model.predict(image_pil.convert("RGB"), text_prompt)
-masks=masks.numpy()[0]
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(config):
+    model = LangSAM()
+    image_pil = Image.open(config.image)
+    text_prompt = config.class_name
+    masks, boxes, phrases, logits = model.predict(image_pil.convert("RGB"), text_prompt)
+    masks=masks.numpy()[0]
 
-image_array=np.array(image_pil)/255.0
-masked_image_array=np.copy(image_array)
+    image_array=np.array(image_pil)/255.0
+    masked_image_array=np.copy(image_array)
+    red_image_array=np.ones_like(masked_image_array,dtype=np.float32)
+    red_image_array[:, :, 1:] = 0
 
-plt.imshow(image_array)
-plt.show()
+    masked_image_array=image_array*(1-masks[:, :, np.newaxis])+red_image_array*masks[:, :, np.newaxis]
 
-red_image_array=np.ones_like(masked_image_array,dtype=np.float32)
-red_image_array[:, :, 1:] = 0
 
-masked_image_array=image_array*(1-masks[:, :, np.newaxis])+red_image_array*masks[:, :, np.newaxis]
+    plt.imshow(masked_image_array)
+    plt.imsave(config.output,masked_image_array)
 
-plt.imshow(masked_image_array)
-plt.show()
+
+
+if __name__ == "__main__":
+    main()
