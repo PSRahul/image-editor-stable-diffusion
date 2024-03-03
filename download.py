8b6003031ce46496deffdbaffed2d71d8c2ee913
PSRahul <psrahul.ml@gmail.com>
2024-03-03 11:40:04 +0100

    started with inpainting

--- download.py
+++ download.py
@@ -5,3 +5,12 @@ from diffusers.utils import load_image, make_image_grid
 pipeline = AutoPipelineForInpainting.from_pretrained(
     "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16, variant="fp16"
 )
+
+import torch
+from diffusers import AutoPipelineForInpainting
+from diffusers.utils import load_image, make_image_grid
+
+pipeline = AutoPipelineForInpainting.from_pretrained(
+    "diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float16, variant="fp16"
+)
+pipeline.enable_model_cpu_offload()
---
+++ download_3.py
@@ -0,0 +1,3 @@
+from diffusers import DiffusionPipeline
+
+pipeline = DiffusionPipeline.from_pretrained("ashawkey/stable-zero123-diffusers",trust_remote_code=True)
